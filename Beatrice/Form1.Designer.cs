d2d6ff8b337dd86e097886f52dec597a02949dde
mtomana <mOrtemawet6>
2015-09-19 14:09:44 +0200

    init2

--- Beatrice/Form1.Designer.cs
+++ Beatrice/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(469, 314);
+            this.ClientSize = new System.Drawing.Size(421, 310);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
